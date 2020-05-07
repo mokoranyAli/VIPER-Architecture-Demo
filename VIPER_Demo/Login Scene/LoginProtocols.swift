7814dc29a944ce549e1d3c387a12765c9c7f5423
Mohamed <mohammed.korany.ali@gmail.com>
2020-05-07 21:28:01 +0200

    first commit

---
+++ VIPER_Demo/Login Scene/LoginProtocols.swift
@@ -0,0 +1,9 @@
+//
+//  LoginProtocols.swift
+//  VIPER_Demo
+//
+//  Created by Mohamed Korany Ali on 5/7/20.
+//  Copyright © 2020 Mohamed Korany Ali. All rights reserved.
+//
+
+import Foundation
---
+++ VIPER_Demo/Router.swift
@@ -0,0 +1,9 @@
+//
+//  Router.swift
+//  VIPER_Demo
+//
+//  Created by Mohamed Korany Ali on 5/3/20.
+//  Copyright © 2020 Mohamed Korany Ali. All rights reserved.
+//
+
+import Foundation
